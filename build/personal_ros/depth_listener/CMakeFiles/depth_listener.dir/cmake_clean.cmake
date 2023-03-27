27d20c50aff2c05bfd1775fa44b6e2956e60dd96
Dang Hong Ky <danghongky.redflag@gmail.com>
2023-03-27 22:35:03 +0700

    Build the first 3d viewer

---
+++ build/personal_ros/depth_listener/CMakeFiles/depth_listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/redflag/catkin_ws/devel/lib/depth_listener/depth_listener"
+  "/home/redflag/catkin_ws/devel/lib/depth_listener/depth_listener.pdb"
+  "CMakeFiles/depth_listener.dir/src/depth.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/depth_listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
